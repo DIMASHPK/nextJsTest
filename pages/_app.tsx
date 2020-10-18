a58ec09c76aa9afabe197971da1afb645404a3fe
dimashpk <shupimonstr@gmail.com>
2020-10-18 15:46:29 +0300

    added nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { Store } from "../redux/store";
+import NextNprogress from "nextjs-progressbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <Provider store={Store}>
+            <NextNprogress color="#006064" />
             <Component {...pageProps} />
             <ToastContainer autoClose={false} />
         </Provider>
